6ea602763432d1b1beddbf2b05993782908f8e64
Jim Totland <jimtotland@Jims-MacBook-Pro.local>
2020-10-17 11:49:07 +0200

    just added tau as an input to algorithm

--- Projects/Project2/project2vs.py
+++ Projects/Project2/project2vs.py
@@ -204,16 +204,14 @@ class Network:
         return gradient
 
     
-def algorithm(I,d,K,h,iterations,function,domain,scaling, alpha, beta):
+def algorithm(I,d,K,h,iterations, tau, function,domain,scaling, alpha, beta):
     """Main training algorithm."""
-    tau = 0.1
       
     Z_0 = generate_input(function,domain,d0,I,d)
     c = get_solution(function,Z_0,d,I,d0)
     if scaling:
         Z_0, a1, b1 = scale_data(alpha,beta,Z_0)
         c, a2, b2 = scale_data(alpha,beta,c)
-    
 
     NN = Network(K,d,I,h,Z_0,c,iterations)
     
@@ -258,10 +256,11 @@ I = 300 # Amount of points ran through the network at once.
 K = 20 # Amount of hidden layers in the network.
 d = 2 # Dimension of the hidden layers in the network. 
 h = 0.1 # Scaling of the activation function application in algorithm.  
-iterations = 1000
+iterations = 1000 #Number of iterations in the Algorithm 
+tau = 0.1 #For the Vanilla Gradient method
 
 #For scaling
-scaling = True
+scaling = False
 alpha = 0.2
 beta = 0.8 
 
@@ -275,11 +274,11 @@ domain = [-2,2]
 def test_function1(x):
     return 0.5*x**2
 
-NN = algorithm(I,d,K,h,iterations,test_function1,domain,scaling, alpha, beta)
+NN = algorithm(I,d,K,h,iterations, tau, test_function1,domain,scaling, alpha, beta)
 test_input = generate_input(test_function1,domain,d0,I,d)
 
 #The a's and b's are for potential scaling fo the data
-output, a1, b1, a2, b2 = testing(NN, test_input, test_function1, domain, d0, d, I, True, alpha, beta)
+output, a1, b1, a2, b2 = testing(NN, test_input, test_function1, domain, d0, d, I, scaling, alpha, beta)
 plot_graph_and_output(output, test_input, test_function1, domain, d0,d, scaling, alpha, beta, a1, b1, a2, b2)
 
 #================#
@@ -291,7 +290,7 @@ domain = [-2,2]
 def test_function2(x):
     return 1 - np.cos(x)
 
-NN = algorithm(I,d,K,h,iterations,test_function2,domain, scaling, alpha, beta)
+NN = algorithm(I,d,K,h,iterations,tau, test_function2,domain, scaling, alpha, beta)
 test_input = generate_input(test_function2,domain,d0,I,d)
 
 #The a's and b's are for potential scaling fo the data
@@ -309,7 +308,7 @@ domain = [[-2,2],[-2,2]]
 def test_function3(x,y):
     return 0.5*(x**2 + y**2)
 
-NN = algorithm(I,d,K,h,iterations,test_function3,domain,scaling,alpha,beta)
+NN = algorithm(I,d,K,h,iterations, tau, test_function3,domain,scaling,alpha,beta)
 test_input = generate_input(test_function3,domain,d0,I,d)
 
 #The a's and b's are for potential scaling fo the data
@@ -328,7 +327,7 @@ domain = [[-2,2],[-2,2]]
 def test_function4(x,y):
     return -1/np.sqrt(x**2 + y**2)
 
-NN = algorithm(I,d,K,h,iterations,test_function4,domain,scaling,alpha,beta)
+NN = algorithm(I,d,K,h,iterations, tau, test_function4,domain,scaling,alpha,beta)
 test_input = generate_input(test_function4,domain,d0,I,d)
 
 #The a's and b's are for potential scaling fo the data
