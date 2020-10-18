b4a831cc1f85b5feb6b89ece6b2b138470354a70
Jim Totland <jimtotland@Jims-MacBook-Pro.local>
2020-10-18 13:35:29 +0200

    minor changes in tests

--- Projects/Project2/project2vs.py
+++ Projects/Project2/project2vs.py
@@ -245,7 +245,7 @@ def algorithm_sgd(I,d,K,h,iterations, tau, chunk, function,domain,scaling, alpha
         
         NN.forward_function()
         gradient = NN.back_propagation()
-        NN.theta.update_parameters(gradient,"vanilla",tau,j)
+        NN.theta.update_parameters(gradient,"adams",tau,j)
 
         if counter < I/chunk - 1:
             NN.Z_list[0,:,:] = input[:,chunk*(counter+1):chunk*(counter+2)]
@@ -291,7 +291,7 @@ beta = 0.8
 #Test function 1 #
 #================#
 
-
+"""
 d0 = 1 # Dimension of the input layer. 
 domain = [-2,2]
 chunk = int(I/10)
@@ -304,41 +304,42 @@ test_input = generate_input(test_function1,domain,d0,I,d)
 #The a's and b's are for potential scaling fo the data
 output, a1, b1, a2, b2 = testing(NN, test_input, test_function1, domain, d0, d, I, scaling, alpha, beta)
 plot_graph_and_output(output, test_input, test_function1, domain, d0,d, scaling, alpha, beta, a1, b1, a2, b2)
-
+"""
 #================#
 #Test function 2 #
 #================#
 """
 d0 = 1 # Dimensin of the input layer. 
 domain = [-2,2]
+chunk = int(I/10)
 def test_function2(x):
     return 1 - np.cos(x)
 
-NN = algorithm(I,d,K,h,iterations,tau, test_function2,domain, scaling, alpha, beta)
+NN = algorithm_sgd(I,d,K,h,iterations,tau,chunk, test_function2,domain, scaling, alpha, beta)
 test_input = generate_input(test_function2,domain,d0,I,d)
 
 #The a's and b's are for potential scaling fo the data
 output, a1, b1, a2, b2 = testing(NN, test_input, test_function2, domain, d0, d, I, scaling, alpha, beta)
 plot_graph_and_output(output, test_input, test_function2, domain, d0,d, scaling, alpha, beta, a1, b1, a2, b2)
-
 """
+
 #================#
 #Test function 3 #
 #================#
-"""
+
 d0 = 2
 d = 4
 domain = [[-2,2],[-2,2]]
+chunk = int(I/10)
 def test_function3(x,y):
     return 0.5*(x**2 + y**2)
 
-NN = algorithm(I,d,K,h,iterations, tau, test_function3,domain,scaling,alpha,beta)
+NN = algorithm_sgd(I,d,K,h,iterations, tau, chunk, test_function3,domain,scaling,alpha,beta)
 test_input = generate_input(test_function3,domain,d0,I,d)
 
 #The a's and b's are for potential scaling fo the data
 output, a1, b1, a2, b2 = testing(NN, test_input, test_function3, domain, d0, d, I, scaling, alpha, beta)
 plot_graph_and_output(output, test_input, test_function3, domain, d0,d, scaling, alpha, beta, a1, b1, a2, b2)
-"""
 
 #================#
 #Test function 4 #
