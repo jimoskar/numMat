0dae40a1540b95b749599f0b83277ea63ca5e417
alexaoh <alex.andunge@gmail.com>
2020-10-25 15:02:56 +0100

    Added more testing functions for systematic testing.

--- Projects/Project2/systematic_tests4_test_functions.py
+++ Projects/Project2/systematic_tests4_test_functions.py
@@ -14,9 +14,9 @@ Used a fixed amount of iterations and images in each test (see report for furthe
 from network_algo import *
 import pickle
 
-def training_test_function1(filename):
+def training_test_function1(filename, sgd = False):
     """Only training the test function 1."""
-    K_values = [10, 15, 20, 23]
+    K_values = [10, 15, 20, 23, 30]
     d_values = [2, 3, 4]
     h_values = [0.05, 0.1, 0.2, 0.3, 0.4]
     iterations = 2000
@@ -52,9 +52,9 @@ def training_test_function1(filename):
     outfile.close()
 
 
-def testing_test_function1(filename, tol = 0.05):
+def testing_test_function1(filename, tol = 0.05, sgd = False):
     """Training and testing random data with test function 1."""
-    K_values = [10, 15, 20, 23]
+    K_values = [10, 15, 20, 23, 30]
     d_values = [2, 3, 4]
     h_values = [0.05, 0.1, 0.2, 0.3, 0.4]
     iterations = 2000
@@ -98,10 +98,93 @@ def testing_test_function1(filename, tol = 0.05):
     pickle.dump(config, outfile)
     outfile.close()
 
-
-def training_test_function3(filename):
+def training_test_function2(filename, sgd = False):
+    """Only training the test function 2."""
     K_values = [10, 15, 20, 23, 30]
-    d_values = [2, 3, 4, 5, 6]
+    d_values = [2, 3, 4]
+    h_values = [0.05, 0.1, 0.2, 0.3, 0.4]
+    iterations = 2000
+    d0 = 1 # Dimension of the input layer. 
+    domain = [-2,2]
+    I = 1000
+    chunk = int(I/1)
+    scaling = False
+    alpha = 0.2
+    beta = 0.8 
+    tau = 1
+    def test_function2(x):
+        return 1 - np.cos(x)
+
+    config = {}
+    i = 1
+    for K in K_values:
+        config[K] = {}
+        for d in d_values:
+            config[K][d] = {}
+            for h in h_values:
+                NN = algorithm(I,d,d0,K,h,iterations,tau, chunk, test_function2, domain, scaling, alpha, beta)
+                config[K][d][h] = NN.J_last # add the value of J in the last iteration to the hash table.
+                               
+                print(i)
+                del NN # Safety measure to avoid leak. 
+                i += 1
+
+    # Save data to file.
+    filenm = 'data/'+filename
+    outfile = open(filenm,'wb')
+    pickle.dump(config, outfile)
+    outfile.close()
+
+def testing_test_function2(filename, tol = 0.05, sgd = False):
+    """Training and testing random data with test function 2."""
+    K_values = [10, 15, 20, 23, 30]
+    d_values = [2, 3, 4]
+    h_values = [0.05, 0.1, 0.2, 0.3, 0.4]
+    iterations = 2000
+    d0 = 1 # Dimension of the input layer. 
+    domain = [-2,2]
+    I = 1000
+    chunk = int(I/1)
+    scaling = False
+    alpha = 0.2
+    beta = 0.8 
+    tau = 1
+    def test_function2(x):
+        return 1 - np.cos(x)
+
+    config = {}
+    i = 1
+    for K in K_values:
+        config[K] = {}
+        for d in d_values:
+            config[K][d] = {}
+            for h in h_values:
+                NN = algorithm(I,d,d0,K,h,iterations,tau, chunk, test_function2, domain, scaling, alpha, beta) 
+                test_input = generate_input(test_function2,domain,d0,I,d)
+                testing(NN, test_input, test_function2, domain, d0, d, I, scaling, alpha, beta)
+
+                # Find amount of correctly classified points (within some tolerance).
+                placeholder = np.array([NN.Y, NN.c])
+                diff = np.diff(placeholder, axis = 0)
+                ratio = len(diff[abs(diff)<tol])/len(diff[0])
+
+                # Add the convergence (last value) of the objective function and the ratio of correctly classified points to testing output.
+                config[K][d][h] = [NN.J(), ratio]
+                               
+                print(i)
+                del NN # Safety measure to avoid leak. 
+                i += 1
+
+    # Save data to file. 
+    filenm = 'data/'+filename
+    outfile = open(filenm,'wb')
+    pickle.dump(config, outfile)
+    outfile.close()
+
+def training_test_function3(filename, sgd = False):
+    """Only training the test function 3."""
+    K_values = [10, 15, 20, 23, 30]
+    d_values = [2, 3, 4]
     h_values = [0.05, 0.1, 0.2, 0.3, 0.4]
     iterations = 2000
     d0 = 2
@@ -112,6 +195,10 @@ def training_test_function3(filename):
     alpha = 0.2
     beta = 0.8 
     I = 1000
+    scaling = False
+    alpha = 0.2
+    beta = 0.8 
+    tau = 1
 
     def test_function3(x,y):
         return 0.5*(x**2 + y**2)
@@ -137,9 +224,10 @@ def training_test_function3(filename):
     pickle.dump(config, outfile)
     outfile.close()
 
-def testing_test_function3(filename, tol):
+def testing_test_function3(filename, tol = 0.05, sgd = False):
+    """Training and testing random data with test function 3."""
     K_values = [10, 15, 20, 23, 30]
-    d_values = [2, 3, 4, 5, 6]
+    d_values = [2, 3, 4]
     h_values = [0.05, 0.1, 0.2, 0.3, 0.4]
     iterations = 2000
     d0 = 2
@@ -150,6 +238,7 @@ def testing_test_function3(filename, tol):
     alpha = 0.2
     beta = 0.8 
     I = 1000
+    tau = 1
 
     def test_function3(x,y):
         return 0.5*(x**2 + y**2)
@@ -182,3 +271,95 @@ def testing_test_function3(filename, tol):
     outfile = open(filenm,'wb')
     pickle.dump(config, outfile)
     outfile.close()
+
+def training_test_function4(filename, sgd = False):
+    """Only training the test function 4."""
+    K_values = [10, 15, 20, 23, 30]
+    d_values = [2, 3, 4]
+    h_values = [0.05, 0.1, 0.2, 0.3, 0.4]
+    iterations = 2000
+    d0 = 2
+    d = 4
+    domain = [[-2,2],[-2,2]]
+    chunk = int(I/10)
+    scaling = False
+    alpha = 0.2
+    beta = 0.8 
+    I = 1000
+    scaling = False
+    alpha = 0.2
+    beta = 0.8 
+    tau = 1
+
+    def test_function4(x,y):
+        return -1/np.sqrt(x**2 + y**2)
+
+    config = {}
+    i = 1
+    for K in K_values:
+        config[K] = {}
+        for d in d_values:
+            config[K][d] = {}
+            for h in h_values:
+                NN = algorithm(I,d,d0,K,h,iterations,tau, chunk, test_function4, domain, scaling, alpha, beta) # This only tests convergence when training. 
+                # The below adds the J_list from training. 
+                config[K][d][h] = NN.J_last # add the value of J in the last iteration to the hash table.
+                                
+                print(i)
+                del NN # Safety measure to avoid leak. 
+                i += 1
+
+    # Save data to file. 
+    filenm = 'data/'+filename
+    outfile = open(filenm,'wb')
+    pickle.dump(config, outfile)
+    outfile.close()
+
+def testing_test_function4(filename, tol):
+    """Training and testing random data with test function 4."""
+    K_values = [10, 15, 20, 23, 30]
+    d_values = [2, 3, 4]
+    h_values = [0.05, 0.1, 0.2, 0.3, 0.4]
+    iterations = 2000
+    d0 = 2
+    d = 4
+    domain = [[-2,2],[-2,2]]
+    chunk = int(I/10)
+    scaling = False
+    alpha = 0.2
+    beta = 0.8 
+    I = 1000
+    tau = 1
+
+    def test_function4(x,y):
+        return -1/np.sqrt(x**2 + y**2)
+
+    config = {}
+    i = 1
+    for K in K_values:
+        config[K] = {}
+        for d in d_values:
+            config[K][d] = {}
+            for h in h_values:
+                NN = algorithm(I,d,d0,K,h,iterations,tau, chunk, test_function4, domain, scaling, alpha, beta) 
+                test_input = generate_input(test_function4,domain,d0,I,d)
+                testing(NN, test_input, test_function4, domain, d0, d, I, scaling, alpha, beta)
+
+                # Find amount of correctly classified points (within some tolerance).
+                placeholder = np.array([NN.Y, NN.c])
+                diff = np.diff(placeholder, axis = 0)
+                ratio = len(diff[abs(diff)<tol])/len(diff[0])
+            
+                # Add the convergence (last value) of the objective function and the ratio of correctly classified points to testing output.
+                config[K][d][h] = [NN.J(), ratio]
+                                
+                print(i)
+                del NN # Safety measure to avoid leak. 
+                i += 1
+
+    # Save data to file. 
+    filenm = 'data/'+filename
+    outfile = open(filenm,'wb')
+    pickle.dump(config, outfile)
+    outfile.close()
+    
