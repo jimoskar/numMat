Minor changes to hamilt probs (should be correct gradients now).

Projects/Project2/hamiltonian_problems_ex.py
@@ -1,7 +1,6 @@
 """The supplied examples of separable Hamiltonian problems tested."""
 import numpy as np
 
-# Nonlinear pendulum.
 def H(p,q):
     """Maybe split into T and V for all these problems."""
     g = 9.81
@@ -35,10 +34,10 @@ class Kepler:
         self.domain_V = domain_V
         
     def T(self, p):
-        return 0.5*(p[0]**2 + p[1]**2)
+        return 0.5*np.inner(p,p)
 
     def grad_T(self, p):
-        return np.array([p[0],p[1]])
+        return np.array([p[0], p[1]])
 
     def V(self, q):
         return -1/np.sqrt(q[0]**2 + q[1]**2)
@@ -48,7 +47,6 @@ class Kepler:
         comp2 = q[1]/np.power(q[0]**2 + q[1]**2, 3/2)
         return np.array([comp1, comp2])
 
-
 class Henon_Heiles:
     """Henon-Heiles problem."""
     def __init__(self, domain_T, domain_V):
@@ -56,17 +54,16 @@ class Henon_Heiles:
         self.domain_V = domain_V
         
     def T(self, p):
-        return 0.5*(p[0]**2 + p[1]**2)
+        return 0.5*np.inner(p,p)
     
     def grad_T(self, p):
         return np.array([p[0], p[1]])
 
     def V(self, q):
-        return 0.5*(q[0]**2 + q[1]**2) + q[0]**2*q[1] - (1/3)*q[1]**3
+        return 0.5*np.inner(q,q) + q[0]**2*q[1] - (1/3)*q[1]**3
     
     def grad_V(self, q):
         comp1 = q[0] + 2*q[0]*q[1]
-        comp2 = q[1] + q[0]**2 - q[1]
+        comp2 = q[1] + q[0]**2 - q[1]**2
 
         return np.array([comp1,comp2])
-