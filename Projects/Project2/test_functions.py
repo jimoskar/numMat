5a8af1e36d8c341665cd394a5ec6f8c087903079
alexaoh <47002854+alexaoh@users.noreply.github.com>
2020-10-30 10:48:32 +0100

    Implement the numerical methods. (#5)
    * Began implementing the numerical methods.
    
    * Made slight change in formula.
    
    * Tried to implement the numerical solvers, but trouble with indexing.
    
    * Began writing the given sep Hamiltonian examples 4 testing.
    
    * Continued implementin num methods.
    
    * Tried to test the implementation of Euler (Wrong)
    
    * added linear pendelum example and fixed some issues in symplectic euler
    
    * changed some names in fixed some minor issues in forwardfunction and backprop. Also fixed some issues related to scaling.
    
    * added some plotting of the exact vs network solution, as well as plotting of the hamiltionian along the trajectory.Also made the reshaped w in Hamiltionian_grad() to be local in that function
    
    * Added plot and savefig parameters to algoSGD
    
    * Made small change with input variable.
    
    * Trying to implement Henon_Heiles and Kepler. Made new function for calculating output from network from a single point.
    
    * added comparison of vanilla and ADAM method
    
    * made a change in get_solution to make it fit with Henon_Heiles
    
    * implemented stormer verlet for exact and network gradient
    
    * fixed minor issues related to the testing functions
    
    * made the 2dim testfunction take 1 vector input instead of 2
    
    * Starte training a network for the given data with unknown hanilton
    
    * Added some more in test_given_data, seems to be working ish
    
    * Made some slight changes to hamiltonina_probs (code quality)
    
    * minor changes
    
    * Minor changes to hamilt probs (should be correct gradients now).
    
    * transposed gradients in kepler
    
    * Made changes to test_given_data. Added ratio to fulfil item in task list
    
    * Added file for testing out scaling
    
    * added correct trajectory data
    
    Co-authored-by: Jim Totland <jimtotland@dhcp-10-24-3-67.wlan.ntnu.no>
    Co-authored-by: Jim Totland <jimtotland@dhcp-10-24-3-118.wlan.ntnu.no>
    Co-authored-by: Jim Totland <jimtotland@Jims-MacBook-Pro.local>
    Co-authored-by: Jim Totland <jimtotland@dhcp-10-24-0-71.wlan.ntnu.no>

--- Projects/Project2/test_functions.py
+++ Projects/Project2/test_functions.py
@@ -1,11 +1,12 @@
 """Train networks and run tests on the four provided test functions."""
 from network_algo import *
+from test_model import *
 
 I = 1000 # Amount of points ran through the network at once. 
 K = 20 # Amount of hidden layers in the network.
 d = 2 # Dimension of the hidden layers in the network. 
 h = 0.05 # Scaling of the activation function application in algorithm.  
-iterations = 2000 #Number of iterations in the Algorithm 
+iterations = 1000 #Number of iterations in the Algorithm 
 tau = 0.1 #For the Vanilla Gradient method
 
 #For scaling
@@ -24,7 +25,7 @@ chunk = int(I/10)
 def test_function1(x):
     return 0.5*x**2
 
-NN = algorithm(I,d,d0, K,h,iterations, tau, chunk, test_function1,domain,scaling, alpha, beta)
+NN = algorithm_sgd(I,d,d0, K,h,iterations, tau, chunk, test_function1,domain,scaling, alpha, beta,plot = True)
 test_input = generate_input(test_function1,domain,d0,I,d)
 
 #The a's and b's are for potential scaling fo the data
@@ -52,13 +53,15 @@ plot_graph_and_output(output, test_input, test_function2, domain, d0,d, scaling,
 #================#
 #Test function 3 #
 #================#
+
 """
+
 d0 = 2
 d = 4
 domain = [[-2,2],[-2,2]]
 chunk = int(I/10)
-def test_function3(x,y):
-    return 0.5*(x**2 + y**2)
+def test_function3(x):
+    return 0.5*(x[0]**2 + x[1]**2)
 
 NN = algorithm(I,d,d0, K,h,iterations, tau, chunk, test_function3,domain,scaling,alpha,beta)
 test_input = generate_input(test_function3,domain,d0,I,d)
@@ -71,14 +74,14 @@ plot_graph_and_output(output, test_input, test_function3, domain, d0,d, scaling,
 #================#
 #Test function 4 #
 #================#
-"""
 
+"""
 d0 = 2
 d = 4
 domain = [[-2,2],[-2,2]]
 chunk = int(I/10)
-def test_function4(x,y):
-    return -1/np.sqrt(x**2 + y**2)
+def test_function4(x):
+    return -1/np.sqrt(x[0]**2 + x[1]**2)
 
 NN = algorithm(I,d,d0, K,h,iterations, tau, chunk, test_function4,domain,scaling,alpha,beta)
 test_input = generate_input(test_function4,domain,d0,I,d)
---
+++ Projects/Project2/test_numerical_methods.py
@@ -0,0 +1,171 @@
+"""Test the numerical methods."""
+from numerical_methods import *
+from import_data import *
+from hamiltonian_problems_ex import *
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+
+
+#====================#
+# Nonlinear Pendulum #
+#====================#
+
+"""
+domain_T = domain_V = [-1, 1]
+pendulum = Pendulum(domain_T, domain_V)
+d0 = 1
+I = 1000
+d = 4
+K = 23
+h = 0.1
+iterations = 5000
+tau = alpha = beta = None
+scaling = False
+#alpha = 0.2
+#beta = 0.8
+#scaling = True
+chunk = int(I/10)
+
+# Train the neural networks. 
+NNT = algorithm_sgd(I, d, d0, K, h, iterations, tau, chunk, pendulum.T, pendulum.domain_T, scaling, alpha, beta)
+NNV = algorithm_sgd(I, d, d0, K, h, iterations, tau, chunk, pendulum.V, pendulum.domain_V, scaling, alpha, beta)
+
+
+#Initial position and momentum
+q0 = np.array([0.2])
+p0 = np.array([0.2])
+times = np.linspace(0, 10, 1000)
+
+
+#Network and exact solution. 'Exact' refers to that the gradient can be computed exactly.
+network_sol, times = symplectic_euler_network(NNT, NNV, p0, q0, times,d0)
+exact_sol, times= symplectic_euler_exact(p0, q0, times, pendulum.grad_T, pendulum.grad_V, d0)
+
+
+#Plotting the exact position/impulse and the network position/impulse resulting from symplectic Euler
+plt.plot(times, network_sol[0,:], linewidth = 0.5, label = "network")
+plt.plot(times, exact_sol[0,:], linewidth = 0.5, label = "exact")
+plt.legend()
+plt.show()
+
+
+#Plotting the hamiltonian. It should be constant along the exact solution.
+#q_data = embed_data(network_sol[0,:],d)
+#p_data = embed_data(network_sol[1,:],d)
+network_ham = NNT.calculate_output(network_sol[1,:].reshape(1,1000)) + NNV.calculate_output(network_sol[0,:].reshape(1,1000))
+#print(p_data.shape)
+#print(NNT.calculate_output(p_data).shape)
+#print(network_ham.shape)
+plt.plot(times, network_ham, label = "network", color = "red", linestyle = "dashed")
+
+
+exact_ham = pendulum.T(exact_sol[1,:])+pendulum.V(exact_sol[0,:])
+#plt.plot(times,pendulum.T(network_sol[1,:])+pendulum.V(network_sol[0,:]), label = "exact")
+#Hamilton = pendulum.T(exact_sol[1,:]) + pendulum.V(exact_sol[0,:])
+plt.plot(times, exact_ham, color = "blue", label = "num method (sympl euler)")
+plt.axhline(y = pendulum.T(p0)+pendulum.V(q0), color = "yellow", label = "anal")
+plt.title("Hamiltonian function")
+plt.legend()
+plt.show()
+"""
+#=========================#
+# Kepler Two-body Problem #
+#=========================#
+"""
+domain_T = domain_V = [[-2, 2], [-2, 2]]
+kepler = Kepler(domain_T, domain_V)
+d0 = 2
+I = 1000
+d = 4
+K = 30
+h = 0.1
+iterations = 2000
+tau = alpha = beta = None
+scaling = False
+chunk = int(I/10)
+
+
+# Train the neural networks. 
+NNT = algorithm_sgd(I, d, d0, K, h, iterations, tau, chunk, kepler.T, kepler.domain_T, scaling, alpha, beta)
+NNV = algorithm_sgd(I, d, d0, K, h, iterations, tau, chunk, kepler.V, kepler.domain_V, scaling, alpha, beta)
+
+input_T = generate_input(kepler.T, kepler.domain_T, d0, I, d)
+input_V = generate_input(kepler.V, kepler.domain_V, d0, I, d)
+
+q0  = np.array([100,100])
+p0 = np.array([0.2,0.3])
+times = np.linspace(0, 40, 10000)
+
+network_sol, times = symplectic_euler_network(NNT, NNV, q0, p0, times, d0)
+exact_sol, times = stormer_verlet_exact(q0, p0, times, kepler.grad_T, kepler.grad_V, d0)
+
+plt.plot(times, network_sol[0,:])
+plt.plot(times, exact_sol[0,:])
+plt.show()
+
+
+ax = plt.axes(projection="3d")# Data for a three-dimensional line
+zline = times #time
+xline = exact_sol[0,:]
+yline = exact_sol[1,:]
+ax.plot3D(xline, yline, zline, "gray")# Data for three-dimensional 
+xnet = network_sol[0,:]
+ynet = network_sol[1,:]
+ax.plot3D(xnet, ynet, zline, "red", label = "network") 
+plt.legend()
+plt.show()
+
+
+"""
+#======================#
+# Henon-Heiles Problem #
+#======================#
+"""
+domain_T = domain_V = [[-2, 2], [-2, 2]]
+HH = Henon_Heiles(domain_T, domain_V)
+d0 = 2
+I = 1000
+d = 4
+K = 30
+h = 0.1
+iterations = 5000
+tau = alpha = beta = None
+scaling = False
+chunk = int(I/10)
+
+# Train the neural networks. 
+NNT = algorithm_sgd(I, d, d0, K, h, iterations, tau, chunk, HH.T, HH.domain_T, scaling, alpha, beta)
+NNV = algorithm_sgd(I, d, d0, K, h, iterations, tau, chunk, HH.V, HH.domain_V, scaling, alpha, beta)
+
+q0  = p0 = np.array([0.2,0.2])
+times = np.linspace(0, 10, 1000)
+
+network_sol, times = stormer_verlet_network(NNT, NNV, q0, p0, times, d0)
+exact_sol, times = stormer_verlet_exact(q0, p0, times, HH.grad_T, HH.grad_V, d0)
+
+plt.plot(times, exact_sol[0,:], label="Exact")
+plt.plot(times, network_sol[0,:], label="Network")
+plt.legend()
+plt.show()
+
+ax = plt.axes(projection="3d")# Data for a three-dimensional line
+zline = times #time
+xline = exact_sol[0,:]
+yline = exact_sol[1,:]
+ax.plot3D(xline, yline, zline, "gray", label = "exact")# Data for three-dimensional 
+xnet = network_sol[0,:]
+ynet = network_sol[1,:]
+ax.plot3D(xnet, ynet, zline, "red", label = "network") 
+
+# Visual changes to the figure. 
+ax.xaxis.pane.fill = False # Remove grey panel orthogonal to x-axis. 
+ax.xaxis.pane.set_edgecolor('white') # Set edgecolor to white. 
+ax.yaxis.pane.fill = False # Remove grey panel orthogonal to t-axis. 
+ax.yaxis.pane.set_edgecolor('white') # Set edgecolor to white. 
+ax.w_zaxis.line.set_lw(0.) # Remove z-axis. 
+ax.set_zticks([]) # Remove ticks from z-axis.
+ax.set_xticks([]) # Remove ticks from x-axis.
+ax.set_yticks([]) # Remove ticks from y-axis.
+plt.legend()
+plt.show()
+"""
